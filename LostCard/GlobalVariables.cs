e4050452db4f37a63d1c57347d93986ff838a5e8
musamthembu84 <musamthembu84@gmail.com>
2018-02-19 22:08:45 +0200

    Class that models cards properties

--- LostCard/GlobalVariables.cs
+++ LostCard/GlobalVariables.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 
 namespace LostCard
 {
     public static class GlobalVariables
     {
-
+        public static HttpClient webApi = new HttpClient();
+        
+        static GlobalVariables()
+        {
+            webApi.BaseAddress = new Uri("http://localhost:52074/api/");
+            webApi.DefaultRequestHeaders.Clear();
+            webApi.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
     }
 }
---
+++ LostCard/Models/mvcCards.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LostCard.Models
+{
+    public class mvcCards
+    {
+        public int ID { get; set; }
+        public string SNumber { get; set; }
+        public Nullable<System.DateTime> DateTime { get; set; }
+        public string Campus { get; set; }
+        public string SMS { get; set; }
+    }
+}
