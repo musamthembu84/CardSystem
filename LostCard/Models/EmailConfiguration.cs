73a33efb3091b80f40a66d161e3924e57f791ceb
musamthembu84 <musamthembu84@gmail.com>
2018-02-22 20:33:31 +0200

    Send Mail using Algorithm for validating status

--- LostCard/Models/EmailConfiguration.cs
+++ LostCard/Models/EmailConfiguration.cs
@@ -31,7 +31,35 @@ namespace LostCard.Models
             return checkStatus;
 
         }
-        
+
+
+        public bool Email(mvcCards Number)
+        {
+            bool stats = false;
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("universityofjohannesburg.card@gmail.com");
+            mailMessage.To.Add(Number.SNumber + "@student.uj.ac.za");
+
+            string body = "Greetings" + System.Environment.NewLine + "Our Notification system at the University of Johannesburg has indicated to us that you have lost your student card." + Environment.NewLine + "" + Number.SNumber + "" + " has been received and it may be collected at the Student Enrolment Center APK Campus for a fee of R10." + System.Environment.NewLine + "Yours Sincerly Admin UJ  APK Campus ";
+
+            mailMessage.Subject = "Collection Of Your Card From University of Johannesburg";
+            mailMessage.Body = body;
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("universityofjohannesburg.card@gmail.com", "streetwise");
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.Send(mailMessage);
+
+            if (isCardValid("universityofjohannesburg.card@gmail.com", "streetwise") == true)
+            {
+                stats = true;
+
+            }
+            return stats;
+        }
+
 
     }
 }
