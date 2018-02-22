67547deab8b6f0a2fb2715ab09eae03b0b084ca0
musamthembu84 <musamthembu84@gmail.com>
2018-02-22 22:47:12 +0200

    Authentication controller created to handle sessions of system

--- LostCard/App_Start/RouteConfig.cs
+++ LostCard/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LostCard
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Auth", action = "LogIn", id = UrlParameter.Optional }
             );
         }
     }
---
+++ LostCard/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LostCard.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View("LogIn");
+        }
+    }
+}
