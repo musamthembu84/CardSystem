e9229f5f3042c6eb23f9225a5c61522a4aba4f14
musamthembu84 <musamthembu84@gmail.com>
2018-02-22 20:30:23 +0200

    Class model created to deal with emails

---
+++ LostCard/Models/EmailConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using OpenPop.Pop3;
+using OpenPop.Mime;
+using OpenPop.Common;
+
+namespace LostCard.Models
+{
+    public class EmailConfiguration
+    {
+
+        private bool isCardValid(string email,string password)
+        {
+            bool localCheck = false;
+            Pop3Client client = new Pop3Client();
+            client.Connect("pop.gmail.com", 995, true);
+            client.Authenticate(email, password);
+
+
+            //get number of newly unread messages
+            int counter = client.GetMessageCount();
+            if (counter >= 1)
+            {
+                client.DeleteAllMessages();
+                localCheck = true;
+                client.Disconnect();
+            }
+            return localCheck;
+
+        }
+        
+
+    }
+}
