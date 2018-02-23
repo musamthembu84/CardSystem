f4e95110262c92ed7af9d65733921e4bb39bfae6
musamthembu84 <musamthembu84@gmail.com>
2018-02-23 21:31:42 +0200

    login auth controller finally updated

--- LostCard/Controllers/AuthController.cs
+++ LostCard/Controllers/AuthController.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LostCard.Models;
 
 namespace LostCard.Controllers
 {
     public class AuthController : Controller
     {
+        private LostCardsEntities db = new LostCardsEntities();
+        
+        private IQueryable<Card> GetCards()
+        {
+            return null;
+        }
         // GET: Auth
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult LogIn()
+        public ActionResult LogIn(mvcAdmin admin)
         {
+           // bool isUser=db.Cards.Any(x=>x.)
             return View("LogIn");
         }
     }
