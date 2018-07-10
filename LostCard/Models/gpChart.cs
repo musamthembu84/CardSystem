89dfd81a1a521c1f96a0f1f85634e204b452dbd2
User <musamthembu84@gmail.com>
2018-07-10 23:47:59 +0200

    gpChart model added

---
+++ LostCard/Models/gpChart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LostCard.Models
+{
+    public class gpChart
+    {
+        public string word { get; set; }
+    }
+}
